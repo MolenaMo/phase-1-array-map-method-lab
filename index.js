1be137a0c6c9c1815f4a70f5b3e429d220fe3d09
Monique Lena Moore <moniquelmoore06@gmail.com>
2022-04-07 20:45:36 -0400

    completed assignment

--- index.js
+++ index.js
@@ -10,7 +10,19 @@ const tutorials = [
   'what is the difference between event capturing and bubbling?',
   'what is JSONP?'
 ];
+//just beneath is the original just as the above was already part of this file.
+// const titleCased = () => {
+//   return tutorials
+// }
 
-const titleCased = () => {
-  return tutorials
+//from here I added: 
+const titleCased=()=> {
+  return tutorials.map((line)=> {
+    const tokens=line.split(" ");
+    const capitalizedTokens=tokens.map(
+(token)=>token.charAt(0).toUpperCase()+token.slice(1));
+const response = capitalizedTokens.join(" ");
+return response
+    
+  })
 }
